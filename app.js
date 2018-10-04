6143cff37d1a1e4f276a6d8f5ece3353935e46fd
Kyle Roach <kroach.work@gmail.com>
2018-10-04 16:34:30 -0400

    style: Prettier

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const boom = require('express-boom');
-const basicAuth = require('express-basic-auth');
+const express = require(`express`);
+const logger = require(`morgan`);
+const bodyParser = require(`body-parser`);
+const boom = require(`express-boom`);
+const basicAuth = require(`express-basic-auth`);
 
-require('dotenv').load();
+require(`dotenv`).load();
 
-const admin = require('firebase-admin');
+const admin = require(`firebase-admin`);
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger(`dev`));
 
 app.use(boom());
 app.use(
@@ -25,14 +25,14 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, `\n`),
   }),
 });
 
-require('./server/routes')(app);
-app.get('*', (_, res) =>
+require(`./server/routes`)(app);
+app.get(`*`, (_, res) =>
   res.status(404).send({
-    message: 'Welcome to the beginning of nothingness.',
+    message: `Welcome to the beginning of nothingness.`,
   })
 );
 
