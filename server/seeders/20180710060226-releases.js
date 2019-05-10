7a64d58d82d92fe79be150be3a242521d21ba774
Kyle Roach <kroach.work@gmail.com>
2019-05-09 23:41:55 -0400

    feat: Add release to seed

--- server/seeders/20180710060226-releases.js
+++ server/seeders/20180710060226-releases.js
@@ -6,9 +6,9 @@ module.exports = {
       `Releases`,
       [
         {
-          title: `September/October 2018`,
-          cover: `https://f001.backblazeb2.com/file/uscchannel/releases/september-october-2018/cover.jpg`,
-          magazine: `https://f001.backblazeb2.com/file/uscchannel/releases/september-october-2018/magazine.pdf`,
+          title: `November/December 2016`,
+          cover: `https://f001.backblazeb2.com/file/uscchannel/releases/november-december-2016/cover.jpg`,
+          magazine: `https://f001.backblazeb2.com/file/uscchannel/releases/november-december-2016/magazine.pdf`,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -20,9 +20,16 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          title: `November/December 2016`,
-          cover: `https://f001.backblazeb2.com/file/uscchannel/releases/november-december-2016/cover.jpg`,
-          magazine: `https://f001.backblazeb2.com/file/uscchannel/releases/november-december-2016/magazine.pdf`,
+          title: `September/October 2018`,
+          cover: `https://f001.backblazeb2.com/file/uscchannel/releases/september-october-2018/cover.jpg`,
+          magazine: `https://f001.backblazeb2.com/file/uscchannel/releases/september-october-2018/magazine.pdf`,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `February/March 2019`,
+          cover: `https://f001.backblazeb2.com/file/uscchannel/releases/february-march-2019/cover.jpg`,
+          magazine: `https://f001.backblazeb2.com/file/uscchannel/releases/february-march-2019/magazine.pdf`,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
